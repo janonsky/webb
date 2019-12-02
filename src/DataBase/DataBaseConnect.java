df7bb77b308c94e0ba6b3af4450c9283e6453b45
Janonsky <ruslan.klimovich@yandex.ru>
2019-12-02 18:15:00 +0300

    Servlet

--- src/DataBase/DataBaseConnect.java
+++ src/DataBase/DataBaseConnect.java
@@ -5,7 +5,7 @@ import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 public class DataBaseConnect {
     private static final String HOST = "localhost";
     private static final String USER = "root";
-    private static final String PASSWORD = "1234";
+    private static final String PASSWORD = "Dalbaeb1337";
     private static final String DB_NAME = "javabd";
 
     public static MysqlDataSource createDataSource() {
